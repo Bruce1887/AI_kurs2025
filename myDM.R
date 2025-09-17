3395282eb03e1c7589281f3cca13f2e045ee602b
OliverHansson <90697655+OliverHansson@users.noreply.github.com>
2025-09-17 15:06:18 +0200

    updated start of a star

--- myDM.R
+++ myDM.R
@@ -35,8 +35,8 @@ neigbouring_cost <- function(roads, x, y) {
   neighbourhood <- list(u=-1, r=-1, d=-1, l=-1)
 
   # fetch up cell (if in bounds)
-  if (y + 1 <= ncol(hroads)) {    # check column bounds
-    neighbourhood$u <- hroads[x, y + 1]
+  if (y + 1 <= ncol(vroads)) {    # check column bounds
+    neighbourhood$u <- vroads[x, y + 1]
   }
   # fetch right cell (if in bounds)
   if (x + 1 <= nrow(hroads)) {    # check row bounds
@@ -44,7 +44,7 @@ neigbouring_cost <- function(roads, x, y) {
   }
   # fetch down cell (if in bounds)
   if (y - 1 >= 1) {
-    neighbourhood$d <- hroads[x, y - 1]
+    neighbourhood$d <- vroads[x, y - 1]
   }
   # fetch left cell (if in bounds)
   if (x - 1 >= 1) {
@@ -56,6 +56,20 @@ neigbouring_cost <- function(roads, x, y) {
 
 a_star <- function(start,target,roads) {
   # TODO: implement A star here 
+  # F cost = G cost (distance from start) + H cost (distance from end)
+  
+  # Computes the heuristic cost (h)
+  manhattan <- function(a, b){
+    return (abs(a[1]-b[1]) + abs(a[2]-b[2]))
+  } 
+
+  frontier <- list(list(
+    x=start[1], y=start[2], g=0,
+    h=manhattan(start, target),
+    f=manhattan(start, target),
+    first_move=NA))
+    
+
   next_move <- 5
   return next_move
 }
