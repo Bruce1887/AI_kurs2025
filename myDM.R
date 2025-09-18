4660e2837a167dee1e827f81bc9b7e2c9b3b9608
bruce1887 <edvinbruce@hotmail.com>
2025-09-18 11:52:26 +0200

    naive implementation. Du styr själv när du inte håller i ett paket. när du håller i ett paket går du automagiskt närmsta vägen

--- myDM.R
+++ myDM.R
@@ -1,10 +1,4 @@
 myFunction <- function(roads, car, packages) { # nolint: object_name_linter.
-  if (car$load == 0) {
-    print("we have not picked up a package")
-  } else {
-    print("we have picked up a package")
-  }
-  
   hroads <- roads$hroads # matrix
   vroads <- roads$vroads # matrix
 
@@ -21,9 +15,12 @@ myFunction <- function(roads, car, packages) { # nolint: object_name_linter.
   if (car$load>0) {
     print(paste("Current load:",car$load))
     print(paste("Destination: X",packages[car$load,3],"Y",packages[car$load,4]))
+    car$nextMove = a_star(list(x=car$x,y=car$y), list(x=packages[car$load,3],y=packages[car$load,4]),roads)
   }
-  car$nextMove=readline("Enter next move. Valid moves are 2,4,6,8,0 (directions as on keypad) or q for quit.")
-  if (car$nextMove=="q") {stop("Game terminated on user request.")}
+  else {
+    car$nextMove=readline("Enter next move. Valid moves are 2,4,6,8,0 (directions as on keypad) or q for quit.")
+    if (car$nextMove=="q") {stop("Game terminated on user request.")}
+  }
   
   car
 }
@@ -56,6 +53,26 @@ neigbouring_cost <- function(roads, x, y) {
 
 a_star <- function(start,target,roads) {
   # TODO: implement A star here 
-  next_move <- 5
-  return next_move
+  next_move <- -1
+
+  #### BEGINNING OF NAIVE IMPLEMENTATION #### 
+  # NAIVE IMPLEMENTATION: Does not take cost of roads into account 
+  diff_x = target$x - start$x
+  diff_y = target$y - start$y
+
+  if (diff_x == 0 && diff_y == 0) {
+    return(0)
+  }
+
+  if(abs(diff_x) > abs(diff_y)) {
+    next_move = ifelse(sign(diff_x) == 1, 6,4)
+  }
+  else {
+    next_move = ifelse(sign(diff_y) == 1, 8,2)
+  }
+  #### END OF NAIVE IMPLEMENTATION ####
+
+  print(paste("next_move",next_move))
+
+  next_move
 }
